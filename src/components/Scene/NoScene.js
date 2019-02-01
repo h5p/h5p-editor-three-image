5001cc91c3ceab32d5bdf949ddd2af525b3c8f66
Frode Petterson <frode.andre.petterson@gmail.com>
2019-02-01 11:15:28 +0100

    HFP-2529 Add no-scene design

--- src/components/Scene/NoScene.js
+++ src/components/Scene/NoScene.js
@@ -4,9 +4,12 @@ import './NoScene.scss';
 export default class NoScene extends React.Component {
   render() {
     return (
-      <div
-        className='h5p-no-scene'
-      >"No scene" placeholder</div>
+      <div className='no-scene-container'>
+        <div className="no-scene-wrapper">
+          <div className="title">No scenes</div>
+          <div className="description">Click a button below to add the first scene</div>
+        </div>
+      </div>
     );
   }
 }

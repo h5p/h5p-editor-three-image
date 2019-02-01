9093d7d9360daf730083cb353783f5158616c28f
Frode Petterson <frode.andre.petterson@gmail.com>
2019-02-01 11:37:38 +0100

    HFP-2529 Fix translations for no scene

--- src/components/Scene/NoScene.js
+++ src/components/Scene/NoScene.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './NoScene.scss';
+import { H5PContext } from "../../context/H5PContext";
 
 export default class NoScene extends React.Component {
   render() {
     return (
       <div className='no-scene-container'>
         <div className="no-scene-wrapper">
-          <div className="title">No scenes</div>
-          <div className="description">Click a button below to add the first scene</div>
+          <div className="title">{ this.context.t('noScenesTitle') }</div>
+          <div className="description">{ this.context.t('noScenesDescription') }</div>
         </div>
       </div>
     );
   }
 }
+
+NoScene.contextType = H5PContext;

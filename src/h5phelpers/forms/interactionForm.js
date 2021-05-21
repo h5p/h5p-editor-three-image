0408930f93dde4e6ad5c33b0d1384273a98a6090
eddiman <edbjorgen@gmail.com>
2021-05-21 14:15:16 +0200

    fully added hiding for when hotspot or opene scene content is toggled in interaction form

--- src/h5phelpers/forms/interactionForm.js
+++ src/h5phelpers/forms/interactionForm.js
@@ -54,31 +54,36 @@ export const createInteractionForm = (field, params, wrapper, parent) => {
   const hiddenInteractionDataSelectors = [
     '.field-name-showAsOpenSceneContent',
   ];
-  /*
-const hotSpotField = interactionDataWrapper.querySelector('.field-name-showAsHotspot')
-const openSceneContentToggle = interactionDataWrapper.querySelector('#field-showasopenscenecontent-39')
-
-  if(openSceneContentToggle.checked) {
-    hotSpotField.style.display = "none"
-  }
-  console.log(openSceneContentToggle)
-  openSceneContentToggle.addEventListener("change", (e) => {
-    if(e.target.checked) {
-      hotSpotField.style.display = "none"
-    } else {
-      hotSpotField.style.display = "initial"
-    }
-  })*/
 
   //Remove fields in that we don't want to show when library is not AdvancedText
-  //TODO: Find a nicer way for this
-  if(!params.action.library.includes("H5P.AdvancedText") &&
-    params.action.library.includes("H5P.Image")) {
+  if(!params.action.library.includes("H5P.AdvancedText")) {
     removeElements(interactionDataWrapper, hiddenInteractionDataSelectors, '')
+  } else {
+    //If library is AdvancedText, then add a listener for dynamically removing the hotspot checkbox when opene scene content is checked.
+    //The hiding/removal of the openSceneContent checkbox when hotspot is toggle is handled by the showWhen widget declared in semantics.json
+    const hotSpotField = interactionDataWrapper.querySelector('.field-name-showAsHotspot')
+    const openSceneContentToggle = interactionDataWrapper.querySelector('.field-name-showAsOpenSceneContent input')
+    hideElementsOnChecked(openSceneContentToggle, hotSpotField)
   }
 
   // Remove semantics that we don't want to show
   removeElements(libraryWrapper, hiddenSemanticsSelectors, ".field.library > ")
+
+};
+
+const hideElementsOnChecked = (checkBox, elementToHide) =>  {
+  //If the element is already checked when the form is created, hide it
+  if(checkBox.checked) {
+    elementToHide.classList.add("do-not-display")
+  }
+  //Add event listener on checkbox for to listen to changes in toggling
+  checkBox.addEventListener("change", (e) => {
+    if(e.target.checked) {
+      elementToHide.classList.add("do-not-display")
+    } else {
+      elementToHide.classList.remove("do-not-display")
+    }
+  })
 
 };
 
